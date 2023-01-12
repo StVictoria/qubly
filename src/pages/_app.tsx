f4c3272b6dd023d1f33bd943f77cc57eb8b4a4bf
Viktoriia <2018vvs@gmail.com>
2023-01-12 12:35:23 +0300

    add preloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.sass'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const loader = document.getElementsByClassName('preloader')[0]
+      loader.classList.remove('preloader_show')
+      setTimeout(() => {
+        if (loader) loader.remove()
+      }, 1000)
+    }
+  }, [])
   return <Component {...pageProps} />
 }
